bda7b923141314c4591b15154aa229cbecdcb8cd
Irina.Rogacheva <irina.rogacheva2018@gmail.com>
2020-04-14 20:26:30 +0300

    17.2.pas

--- 17.2.pas
+++ 17.2.pas
@@ -1,50 +1,53 @@
 PROGRAM ReadN(INPUT, OUTPUT);
 VAR
-  N: INTEGER;
-PROCEDURE ReadDigit(VAR F: TEXT; VAR D: INTEGER);
+  Number: INTEGER;
+
+PROCEDURE ReadDigit(VAR FileFrom: TEXT; VAR Digit: INTEGER);
 {—читывает текущий символ из файла, если он - цифра, возвращает его 
 преобразу€ в значение типа INTEGER. ≈сли считанный символ не цифра
 возвращает -1}
 VAR    
   Ch: CHAR;
-  TempFile: TEXT;
 BEGIN
-  D := -1;
-  REWRITE(TempFile);
-  IF NOT EOLN(F)
+  Digit := -1;
+  IF NOT EOLN(FileFrom)
   THEN
-  BEGIN
-    READ(F, Ch);
-    IF (Ch >= '0') AND (Ch <= '9') 
-    THEN
-      BEGIN
-        WRITELN(TempFile, Ch);
-        RESET(TempFile);
-        READ(TempFile, D);
-      END 
-  END;
+    BEGIN
+      READ(FileFrom, Ch);
+      IF (Ch = '0') THEN Digit := 0 ELSE
+      IF (Ch = '1') THEN Digit := 1 ELSE
+      IF (Ch = '2') THEN Digit := 2 ELSE
+      IF (Ch = '3') THEN Digit := 3 ELSE
+      IF (Ch = '4') THEN Digit := 4 ELSE
+      IF (Ch = '5') THEN Digit := 5 ELSE
+      IF (Ch = '6') THEN Digit := 6 ELSE
+      IF (Ch = '7') THEN Digit := 7 ELSE
+      IF (Ch = '8') THEN Digit := 8 ELSE
+      IF (Ch = '9') THEN Digit := 9 ELSE
+      Digit := -1
+    END  
 END;  
   
-PROCEDURE ReadNumber(VAR F: TEXT; VAR N: INTEGER);
+PROCEDURE ReadNumber(VAR FileFrom: TEXT; VAR Number: INTEGER);
 VAR
-  D: INTEGER;
+  Digit: INTEGER;
 BEGIN
-  N := 0;  
-  ReadDigit(F, D);
-  WHILE (D <> -1) AND (N <> -1)
+  Number := 0;  
+  ReadDigit(FileFrom, Digit);
+  WHILE (Digit <> -1) AND (Number <> -1)
   DO
     BEGIN   
-      IF ((MAXINT DIV 10) > N) OR (((MAXINT DIV 10) = N) AND ((MAXINT MOD 10) >= D))
+      IF ((MAXINT DIV 10) > Number) OR (((MAXINT DIV 10) = Number) AND ((MAXINT MOD 10) >= Digit))
       THEN
         BEGIN
-          N := N*10 + D;
-          ReadDigit(F, D)
+          Number := Number*10 + Digit;
+          ReadDigit(FileFrom, Digit)
         END  
       ELSE
-        N := -1 
+        Number := -1 
     END;    
-  END;
+END; 
 BEGIN
-  ReadNumber(INPUT, N);
-  WRITELN(N) 
+  ReadNumber(INPUT, Number);
+  WRITELN(Number) 
 END.       
