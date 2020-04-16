3efae394a888d83d23a7f43c17134f0e76f3e8f2
Irina.Rogacheva <irina.rogacheva2018@gmail.com>
2020-04-16 21:05:48 +0300

    17.3.pas

--- lw17/17.3.pas
+++ lw17/17.3.pas
@@ -1,6 +1,6 @@
 PROGRAM Stat(INPUT, OUTPUT);
 VAR
-  N1, N2, Min, Max, Sum, Q, Average, FractPart: INTEGER;
+  Number, Min, Max, Sum, Quantity, Average, FractPart: INTEGER;
   Overflow: BOOLEAN;
 PROCEDURE ReadDigit(VAR FileFrom: TEXT; VAR Digit: INTEGER);
 VAR    
@@ -42,59 +42,42 @@ BEGIN
       ELSE
         Number := -1 
     END;    
-END;   
+END; 
+  
 BEGIN  
-  ReadNumber(INPUT, N1);
-  IF N1 <> -1
+  ReadNumber(INPUT, Number);
+  IF Number <> -1
   THEN
     BEGIN  
-      Sum := N1;
-      Q := 1;
-      Min := N1;
-      Max := N1;
-      ReadNumber(INPUT, N2);
-      IF (MAXINT >= N1 + N2) AND (N2 <> -1) 
-      THEN
+      Sum := Number;
+      Quantity := 1;
+      Min := Number;
+      Max := Number;
+      Overflow := FALSE;          
+      WHILE NOT EOLN(INPUT) AND (NOT Overflow)
+      DO
         BEGIN
-          Sum := Sum + N2;
-          Q := Q + 1;
-          IF N1 < N2
+          ReadNumber(INPUT, Number);
+          IF (Sum + Number <= MAXINT) AND (Number <> -1)
           THEN
             BEGIN
-              Min := N1;
-              Max := N2
-            END  
-          ELSE
-            BEGIN
-              Min := N2;
-              Max := N1
-            END; 
-          Overflow := FALSE;          
-          WHILE NOT EOLN(INPUT) AND (NOT Overflow)
-          DO
-            BEGIN
-              ReadNumber(INPUT, N1);
-              IF (Sum + N1 <= MAXINT) AND (N1 <> -1)
+              Quantity := Quantity + 1;   
+              Sum := Sum + Number;
+              IF Number < Min
               THEN
-                BEGIN
-                  Q := Q + 1;   
-                  Sum := Sum + N1;
-                  IF N1 < Min
-                  THEN
-                    Min := N1;
-                  IF N1 > Max
-                  THEN
-                    Max := N1;
-                END
-              ELSE
-                Overflow := TRUE  
-            END
-        END
-    END;            
-  Average := Sum DIV Q;
-  FractPart := (Sum MOD Q) * 100 DIV Q;
-  WRITELN('Min is ', Min);
-  WRITELN('Max is ', Max);
-  WRITELN('Average is ', Average, '.', FractPart);
+                Min := Number;
+              IF Number > Max
+              THEN
+                Max := Number;
+            END 
+          ELSE
+            Overflow := TRUE    
+        END;
+      Average := Sum DIV Quantity;
+      FractPart := (Sum MOD Quantity) * 100 DIV Quantity;
+      WRITELN('Min is ', Min);
+      WRITELN('Max is ', Max);
+      WRITELN('Average is ', Average, '.', FractPart);  
+    END              
 END.       
               
