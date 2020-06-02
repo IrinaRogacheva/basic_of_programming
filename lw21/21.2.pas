3986c700cd32a45ac5c770c115f53cbfb0371411
Irina.Rogacheva <irina.rogacheva2018@gmail.com>
2020-06-02 16:39:13 +0300

    21.2.pas

--- lw21/21.2.pas
+++ lw21/21.2.pas
@@ -3,80 +3,80 @@ PROGRAM Encryption(INPUT, OUTPUT);
   и печатает новые символы в файл}
 CONST
   Len = 20;
-  Alphabet = 26;
+  Alphabet = 26 + 1;
+  ChiperSymbols = [' ', 'A' .. 'Z'];
 TYPE
   Str = ARRAY [1 .. Len] OF 'A' .. 'Z';
-  Chiper = ARRAY ['A' .. 'Z'] OF CHAR;
+  Chiper = ARRAY [' ' .. 'Z'] OF CHAR;
 VAR
   Msg: Str;
   Code: Chiper;
   I: INTEGER;
-  K, Ch: CHAR;
+  Ch: CHAR;
   Length: 1 .. Len;
   ChiperFile, ChiperText: TEXT;
   Error: BOOLEAN;
  
-PROCEDURE Encode(VAR S: Str; VAR FileIn: TEXT);
+PROCEDURE Initialize(VAR Code: Chiper; VAR Error: BOOLEAN; VAR ChiperFile: TEXT);
+VAR
+  I: INTEGER;
+{Присвоить Code шифр замены}
+BEGIN {Initialize} 
+  RESET(ChiperFile);
+  I := 0;
+  Error := FALSE;
+  WHILE (NOT EOF(ChiperFile))
+  DO
+    BEGIN
+      READ(ChiperFile, Ch);
+      IF (Ch IN ChiperSymbols) AND (NOT EOLN(ChiperFile)) 
+      THEN
+        BEGIN
+          READ(ChiperFile, Code[Ch]);
+          I := I + 1;
+        END;
+      READLN(ChiperFile)
+    END;
+  IF I < Alphabet
+  THEN
+    BEGIN
+      Error := TRUE; 
+      WRITELN('Ошибка в файле шифров: не все символы могут быть зашифрованы')
+    END           
+END;  {Initialize} 
+ 
+PROCEDURE Encode(VAR S: Str; VAR FileIn: TEXT; Length: INTEGER);
 {Выводит символы из Code, соответствующие символам из S}
 VAR
   Index: 1 .. Len;
 BEGIN {Encode}
+  REWRITE(FileIn);
   FOR Index := 1 TO Length
   DO
-    IF S[Index] IN ['A' .. 'Z']
+    IF S[Index] IN ChiperSymbols
     THEN
       WRITE(FileIn, Code[S[Index]])
     ELSE
-      IF S[Index] = ' '
-      THEN
-        WRITE(FileIn, '!')
-      ELSE
-        WRITE(FileIn, S[Index]);
+      WRITE(FileIn, S[Index]);
   WRITELN(FileIn)
 END;  {Encode}
  
 BEGIN {Encryption}
-  ASSIGN(ChiperFile, 'CHIPER.TXT'); 
-  RESET(ChiperFile);
-  I := 0;
-  WHILE NOT EOLN(ChiperFile) AND (NOT Error)
-  DO
-    BEGIN 
-      I := I + 1; 
-      Length := I;
-      READ(ChiperFile, Ch);
-      IF NOT (Ch IN ['A' .. 'Z'])
-      THEN
-        BEGIN
-          Error := TRUE;
-          WRITELN('Ошибка в файле шифра: он может содержать только буквы')
-        END  
-    END; 
-  IF Length <> Alphabet
-  THEN
-    BEGIN
-      Error := TRUE;
-      WRITELN('Ошибка в файле шифра: количество символов не совпадает с алфавитом')
-    END;     
+  {Инициализировать Code}
+  ASSIGN(ChiperFile, 'Chiper.txt');
+  Initialize(Code, Error, ChiperFile);
   IF NOT Error
   THEN
-    BEGIN 
-      RESET(ChiperFile);     
-      FOR K := 'A' TO 'Z'
-      DO
-        READ(ChiperFile, Code[K]);
-      I := 0;  
+    BEGIN
       WHILE NOT EOLN AND (I < Len)
       DO
         BEGIN
           I := I + 1; 
           Length := I;
-          READ(Msg[I]);
-        END;    
-      ASSIGN(ChiperText, 'CHIPERTEXT.TXT'); 
-      REWRITE(ChiperText);       
-      {распечатать кодированное сообщение}
-      Encode(Msg, ChiperText)
-    END;
-  WRITELN   
+          READ(Msg[I])
+        END;  
+      READLN;     
+      ASSIGN(ChiperText, 'ChiperText.txt');   
+      Encode(Msg, ChiperText, Length)
+    END 
 END.  {Encryption}
