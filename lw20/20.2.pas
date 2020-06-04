5b559b9eb7ebe60e137dd6a0e4f72cd7ec12b172
Irina.Rogacheva <irina.rogacheva2018@gmail.com>
2020-06-04 23:26:49 +0300

    20.2.pas

--- lw20/20.2.pas
+++ lw20/20.2.pas
@@ -3,63 +3,107 @@ CONST
   Min = 1;
   Max = 25;
   First = 'A';
-  Last = 'Z'; 
+  Last = 'Z';
+  MaxLength = 10;
+  NumberOfXColumns = 5; 
 TYPE
-  Letters = First..Last;   
+  Letters = First..Last;
+  MatrixSet = SET OF Min .. Max;
+  LettersArray = ARRAY [1 .. MaxLength] OF Letters;
+  MatrixesArray = ARRAY ['A' .. 'Z'] OF MatrixSet;   
 VAR
-  Ch: CHAR;
-  Letter: Letters; 
   UserSet: SET OF Letters;
-  Symbole: SET OF Min..Max;
-  I, Digit: INTEGER;
+  Length: 1 .. MaxLength;
   MatrixesFile: TEXT;
+  EnteredSymbols: LettersArray;
+  Matrixes: MatrixesArray;
+  
+FUNCTION EnteredString: LettersArray;
+VAR
+  UserString: LettersArray;
+  I: INTEGER;
+  Ch: CHAR;
 BEGIN
-  UserSet := [];  
-  ASSIGN(MatrixesFile, 'MATRIXES.TXT'); 
-  RESET(MatrixesFile);
-  WRITE('¬ведите английские заглавные буквы: ');
-  WHILE NOT EOLN
+  UserSet := []; 
+  I := 1;
+  WHILE NOT EOLN AND (Length <= 10)
   DO
     BEGIN
-      READ(Ch); 
+      READ(Ch);
+      UserString[I] := Ch;
       UserSet := UserSet + [Ch];
-    END;  
+      Length := I;
+      I := I + 1       
+    END;
+  EnteredString := UserString; 
+END;     
+  
+FUNCTION MatrixesFromFile(VAR MatrixesFile: TEXT; EnteredString: LettersArray): MatrixesArray;
+VAR
+  Letter: Letters; 
+  Matrix: MatrixSet;
+  Digit: INTEGER;
+  Matrixes: MatrixesArray;
+BEGIN   
+  RESET(MatrixesFile);   
   FOR Letter := First TO Last
   DO
     BEGIN
-      Symbole := [];
       IF Letter IN UserSet
       THEN
-        BEGIN   
+        BEGIN 
+          Matrix := []; 
           WHILE NOT EOLN(MatrixesFile)
           DO
             BEGIN
-              READ(MatrixesFile, Digit);          
-              Symbole := Symbole + [Digit]
+              READ(MatrixesFile, Digit); 
+              Matrix := Matrix + [Digit];     
             END;
-          FOR I := Min TO Max
-          DO
-            BEGIN
-              IF I IN Symbole  
-              THEN
-                BEGIN 
-                  IF I MOD 5 = 0
-                  THEN
-                    WRITELN('X')
-                  ELSE
-                    WRITE('X')
-                END    
-              ELSE
-                BEGIN 
-                  IF I MOD 5 = 0
-                  THEN
-                    WRITELN(' ')
-                  ELSE
-                    WRITE(' ')
-                END 
-            END;
-          WRITELN    
-        END;  
-      READLN(MatrixesFile)           
-    END    
+          Matrixes[Letter] := Matrix;              
+        END;
+      READLN(MatrixesFile)      
+    END;
+  MatrixesFromFile := Matrixes  
+END;
+
+PROCEDURE XPrintString(EnteredString: LettersArray; Matrixes: MatrixesArray);
+VAR
+  I, J, NumberOfLetter, Count: INTEGER;
+  Letter: Letters; 
+BEGIN    
+  NumberOfLetter := 1;
+  Count := 0;
+  FOR I := 1 TO (Length * NumberOfXColumns)
+  DO
+    BEGIN
+      Letter := EnteredString[NumberOfLetter];
+      FOR J := 1 TO 5
+      DO
+        BEGIN
+          Count := Count + 1;              
+          IF Count IN Matrixes[Letter]
+          THEN
+            WRITE('X')
+          ELSE
+            WRITE(' ');               
+        END;
+      WRITE(' ');
+      Count := Count - NumberOfXColumns;
+      NumberOfLetter := NumberOfLetter + 1;
+      IF ((I MOD Length) = 0)
+      THEN
+        BEGIN
+          WRITELN;
+          Count := Count + NumberOfXColumns;
+          NumberOfLetter := 1;
+        END  
+    END
+END;
+  
+BEGIN
+  ASSIGN(MatrixesFile, 'MATRIXES.TXT');
+  WRITE('¬ведите английские заглавные буквы: ');
+  EnteredSymbols := EnteredString;
+  Matrixes := MatrixesFromFile(MatrixesFile, EnteredSymbols);
+  XPrintString(EnteredSymbols, Matrixes)                 
 END.  
