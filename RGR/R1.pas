e19a41fc75c6f5800db1040b81685c10e6a86907
Irina.Rogacheva <irina.rogacheva2018@gmail.com>
2020-07-08 19:29:35 +0300

    R1.pas

---
+++ RGR/R1.pas
@@ -0,0 +1,37 @@
+PROGRAM CountWords(INPUT, OUTPUT);
+USES
+  WordModule, Container;
+VAR
+  FileIn, FileOut: TEXT;  
+PROCEDURE GetStatistics(VAR FileIn: TEXT; VAR FileOut: TEXT);
+VAR
+  Found: BOOLEAN;
+  NewWord: WordType;
+  Root: Tree;
+BEGIN {GetStatistics}
+  RESET(FileIn);
+  Root := NIL;
+  WHILE NOT EOF(FileIn)
+  DO
+    BEGIN
+      WHILE NOT EOLN(FileIn)
+      DO
+        BEGIN
+          GetWord(FileIn, NewWord, Found);
+          IF Found
+          THEN
+            AddWordInContainer(Root, NewWord);
+        END;
+      READLN(FileIn)      
+    END;
+  REWRITE(FileOut);  
+  CopyContainer(FileOut, Root);
+  ClearContainer(Root);  
+END; {GetStatistics} 
+  
+BEGIN {CountWords}
+  ASSIGN(FileIn, 'FileInForStatistics.txt');
+  ASSIGN(FileOut, 'FileOutForStatistics.txt');
+  GetStatistics(FileIn, FileOut);  
+  WRITELN(FileOut)
+END. {CountWords}  
